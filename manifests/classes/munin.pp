f484b8c66f9a6ee6cbc11aca7c2d41d729df6d25
Alban Peignier <alban@tryphon.eu>
2010-09-15 09:31:40 +0200

    Remove useless ressources

--- manifests/classes/munin.pp
+++
@@ -1,6 +0,0 @@
-class munin-node::local inherits munin-node {
-  # plugins are defined by puppet on boot
-  readonly::mount_tmpfs { "/etc/munin/plugins": }
-  # required by ping_ plugin
-  package { iputils-ping: }
-}
